83f79dc40e6cbe8e7f3b29c317a6126f7c797778
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-13 00:35:31 -0400

    essentials

---
+++ src/main/java/com/gestaoesportiva/handego/HandegoApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoesportiva.handego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandegoApplication.class, args);
+    }
+
+}
