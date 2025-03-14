966c9b8b9dd13984bb5a888fb903ee640aa4be5c
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-14 16:27:58 -0400

    create table campeonato sql

--- src/main/resources/db.migration/V3__create-table-campeonato.sql
+++ src/main/resources/db.migration/V3__create-table-campeonato.sql
@@ -1,14 +1,13 @@
 CREATE TABLE campeonato (
-                            id             BIGSERIAL UNIQUE PRIMARY KEY,
-                            nome           VARCHAR(255) NOT NULL,
-                            descricao      VARCHAR(255) NOT NULL UNIQUE,
-                            dataInicio     VARCHAR(255) NOT NULL,
-                            dataFim        VARCHAR(255) NOT NULL,
-                            local          VARCHAR(255) NOT NULL,
-                            cidade         VARCHAR(255) NOT NULL,
-                            estado         VARCHAR(255) NOT NULL,
-                            federacao_id   BIGINT,
-                            ativo          BOOLEAN NOT NULL,
-
-                            CONSTRAINT fk_federacao_id FOREIGN KEY (federacao_id) REFERENCES federacao(id) ON DELETE SET NULL
+                            id BIGSERIAL PRIMARY KEY,
+                            nome VARCHAR(255) NOT NULL,
+                            descricao TEXT,
+                            data_inicio VARCHAR(255) NOT NULL,
+                            data_fim VARCHAR(255) NOT NULL,
+                            local VARCHAR(255),
+                            cidade VARCHAR(255),
+                            estado VARCHAR(255),
+                            federacao_id BIGINT NOT NULL,
+                            ativo BOOLEAN NOT NULL,
+                            CONSTRAINT fk_federacao FOREIGN KEY (federacao_id) REFERENCES federacao(id)
 );
