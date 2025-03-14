c8b9dda0de5e1f8936bf1e53ae4dbf13a1c7701b
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-14 16:29:49 -0400

    add campeonato service

--- src/main/java/com/gestaoesportiva/handego/application/campeonato/service/CampeonatoService.java
+++ src/main/java/com/gestaoesportiva/handego/application/campeonato/service/CampeonatoService.java
@@ -5,6 +5,9 @@ import com.gestaoesportiva.handego.application.campeonato.dto.CampeonatoResponse
 import com.gestaoesportiva.handego.application.campeonato.dto.CampeonatoUpdate;
 import com.gestaoesportiva.handego.application.campeonato.entity.Campeonato;
 import com.gestaoesportiva.handego.application.campeonato.repository.CampeonatoRepository;
+import com.gestaoesportiva.handego.application.federacao.dto.FederacaoRequest;
+import com.gestaoesportiva.handego.application.federacao.entity.Federacao;
+import com.gestaoesportiva.handego.application.federacao.repository.FederacaoRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,13 +16,18 @@ import org.springframework.stereotype.Service;
 public class CampeonatoService {
 
     private final CampeonatoRepository campeonatoRepository;
+    private final FederacaoRepository federacaoRepository;
 
-    public CampeonatoService(CampeonatoRepository campeonatoRepository) {
+    public CampeonatoService(CampeonatoRepository campeonatoRepository, FederacaoRepository federacaoRepository) {
         this.campeonatoRepository = campeonatoRepository;
+        this.federacaoRepository = federacaoRepository;
     }
 
     public Campeonato create(CampeonatoRequest dados) {
         Campeonato campeonato = new Campeonato(dados);
+        Federacao federacao = federacaoRepository.findById(dados.federacaoId()).orElseThrow(() -> new RuntimeException("Federacao não encontrada"));
+        campeonato.setFederacao(federacao);
+
         campeonatoRepository.save(campeonato);
         return campeonato;
     }
@@ -50,4 +58,5 @@ public class CampeonatoService {
         Campeonato campeonato = campeonatoRepository.findById(id).orElseThrow(() -> new RuntimeException("Campeonato não encontrado"));
         campeonatoRepository.deleteById(id);
     }
+
 }
