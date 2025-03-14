47399c588f1ca84f2168389b09c7f6f610c5d50a
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-14 16:27:52 -0400

    create table federacao sql

--- src/main/resources/db.migration/V2__create-table-federacao.sql
+++ src/main/resources/db.migration/V2__create-table-federacao.sql
@@ -1,6 +1,7 @@
 CREATE TABLE federacao(
                           id       BIGSERIAL UNIQUE PRIMARY KEY,
                           nome     VARCHAR(255) NOT NULL,
+                          sigla     VARCHAR(255) NOT NULL,
                           email    VARCHAR(255) NOT NULL UNIQUE,
                           senha    VARCHAR(255) NOT NULL,
                           telefone VARCHAR(255) NOT NULL,
