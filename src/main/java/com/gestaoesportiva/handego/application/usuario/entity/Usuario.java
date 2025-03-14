7b69dab02c6f6350668a2c32f3cfb5d6d50fa93a
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-14 16:28:34 -0400

    alter usuario class

--- src/main/java/com/gestaoesportiva/handego/application/usuario/entity/Usuario.java
+++ src/main/java/com/gestaoesportiva/handego/application/usuario/entity/Usuario.java
@@ -3,24 +3,23 @@ package com.gestaoesportiva.handego.application.usuario.entity;
 import com.gestaoesportiva.handego.application.usuario.dto.UsuarioRequest;
 import com.gestaoesportiva.handego.application.usuario.dto.UsuarioUpdate;
 import com.gestaoesportiva.handego.domain.enumeration.Role;
-import com.gestaoesportiva.handego.domain.model.EntidadeBase;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "usuario")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Usuario extends EntidadeBase {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
-
     private String email;
     private String senha;
     private String telefone;
@@ -29,7 +28,7 @@ public class Usuario extends EntidadeBase {
     private String cpf;
     private String cidade;
     private String estado;
-    private boolean ativo;
+    private Boolean ativo;
 
     public Usuario(UsuarioRequest dados) {
         this.nome = dados.nome();
@@ -45,10 +44,14 @@ public class Usuario extends EntidadeBase {
 
 
     public void atualizar(UsuarioUpdate dados) {
-        atualizarBase(dados);
-        if (dados.cidade() != null) this.cidade = dados.cidade();
+        if (dados.nome() != null) this.nome = dados.nome();
+        if (dados.email() != null) this.email = dados.email();
+        if (dados.senha() != null) this.senha = dados.senha();
+        if (dados.telefone() != null) this.telefone = dados.telefone();
         if (dados.role() != null) this.role = dados.role();
         if (dados.cpf() != null) this.cpf = dados.cpf();
+        if (dados.cidade() != null) this.cidade = dados.cidade();
+        if (dados.estado() != null) this.estado = dados.estado();
 
     }
 
