edd7723691779481e0f957d1ef92a6f1b4e8322a
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-17 00:40:08 -0400

    feat: added clube entity

--- src/main/java/com/gestaoesportiva/handego/application/clube/entity/Clube.java
+++ src/main/java/com/gestaoesportiva/handego/application/clube/entity/Clube.java
@@ -1,15 +1,19 @@
 package com.gestaoesportiva.handego.application.clube.entity;
 
+import com.gestaoesportiva.handego.application.campeonato.entity.Campeonato;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "clubes")
+@Table
 public class Clube {
 
     @Id
@@ -23,6 +27,9 @@ public class Clube {
     private String telefone;
     private String cnpj;
     private String estado;
+
+    @ManyToMany(mappedBy = "clubes")
+    private Set<Campeonato> campeonatos = new HashSet<>();
 
     private Boolean ativo;
 }
