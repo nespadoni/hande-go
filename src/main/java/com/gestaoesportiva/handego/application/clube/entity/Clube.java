8a6b1cc884e499ff45cd9dca3858cdc4107d08fb
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-19 16:48:19 -0400

    Alter Entity Clube

--- src/main/java/com/gestaoesportiva/handego/application/clube/entity/Clube.java
+++ src/main/java/com/gestaoesportiva/handego/application/clube/entity/Clube.java
@@ -1,12 +1,16 @@
 package com.gestaoesportiva.handego.application.clube.entity;
 
 import com.gestaoesportiva.handego.application.campeonato.entity.Campeonato;
+import com.gestaoesportiva.handego.application.clube.dto.ClubeRequest;
+import com.gestaoesportiva.handego.application.clube.dto.ClubeUpdate;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -29,7 +33,29 @@ public class Clube {
     private String estado;
 
     @ManyToMany(mappedBy = "clubes")
-    private Set<Campeonato> campeonatos = new HashSet<>();
+    private List<Campeonato> campeonatos = new ArrayList<>();
 
     private Boolean ativo;
+
+
+    public Clube(ClubeRequest dados) {
+        this.nome = dados.nome();
+        this.sigla = dados.sigla();
+        this.email = dados.email();
+        this.senha = dados.senha();
+        this.telefone = dados.telefone();
+        this.cnpj = dados.cnpj();
+        this.estado = dados.estado();
+        this.ativo = true;
+    }
+
+    public void update(ClubeUpdate dados) {
+        if (dados.nome() != null) this.nome = dados.nome();
+        if (dados.sigla() != null) this.sigla = dados.sigla();
+        if (dados.email() != null) this.email = dados.email();
+        if (dados.senha() != null) this.senha = dados.senha();
+        if (dados.telefone() != null) this.telefone = dados.telefone();
+        if (dados.cnpj() != null) this.cnpj = dados.cnpj();
+        if (dados.estado() != null) this.estado = dados.estado();
+    }
 }
