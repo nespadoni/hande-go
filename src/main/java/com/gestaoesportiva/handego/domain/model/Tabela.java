b99a601e4b12870559c12d7c256a79446b06d1b5
CARLOS BENITO MARTINS SPADONI NETO <137246816+nespadoni@users.noreply.github.com>
2025-03-27 01:31:58 -0400

    Refactor Tabela class to include entity annotations and manage rodadas and clubes

--- src/main/java/com/gestaoesportiva/handego/domain/model/Tabela.java
+++ src/main/java/com/gestaoesportiva/handego/domain/model/Tabela.java
@@ -1,47 +1,74 @@
-//package com.gestaoesportiva.handego.domain.model;
-//
-//import com.gestaoesportiva.handego.application.clube.entity.Clube;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Table;
-//import lombok.Data;
-//
-//import java.util.ArrayList;
-//
-//@Data
-//@Entity
-//@Table(name = "tabelas")
-//public class Tabela {
-//
-//    private ArrayList<Clube> times = new ArrayList<>();
-//
-//    public void geracaoTabela(Clube id) {
-//        times.add(id);
-//
-//        int numeroTimes = times.size();
-//
-//        if (numeroTimes % 2 != 0) {
-//            times.add("Descanso");
-//            numeroTimes++;
-//        }
-//
-//        int numeroRodadas = numeroTimes - 1;
-//
-//        for (int rodada = 0; rodada < numeroRodadas; rodada++) {
-//            System.out.println("Rodada " + (rodada + 1) + ":");
-//
-//            int jogosExibidos = 0;
-//
-//            for (int i = 0; i < numeroTimes / 2 && jogosExibidos < numeroTimes / 2 - 1; i++) {
-//                int time1 = i;
-//                int time2 = numeroTimes - 1 - i;
-//                if (!times.get(time1).equals("Descanso") && !times.get(time2).equals("Descanso")) {
-//                    System.out.println(times.get(time1) + " vs " + times.get(time2));
-//                }
-//            }
-//
-//            String timeRemovido = times.removeLast();
-//            times.add(1, timeRemovido);
-//        }
-//    }
-//
-//}
+package com.gestaoesportiva.handego.domain.model;
+
+import com.gestaoesportiva.handego.application.clube.entity.Clube;
+import com.gestaoesportiva.handego.application.partida.entity.Partida;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity()
+@Table(name = "tabelas")
+public class Tabela {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tabela_clubes",
+            joinColumns = @JoinColumn(name = "tabela_id"),
+            inverseJoinColumns = @JoinColumn(name = "clube_id")
+    )
+    private ArrayList<Clube> times = new ArrayList<>();
+
+    @OneToMany(mappedBy = "tabela", cascade = CascadeType.ALL)
+    private List<Rodada> rodadas = new ArrayList<>();
+
+    public void adicionarTimes(Clube clube) {
+        times.add(clube);
+    }
+
+    public void gerarRodadas() {
+        List<Clube> listaTimes = new ArrayList<>(times);
+
+        if (listaTimes.size() % 2 != 0) {
+            Clube descanso = new Clube();
+            descanso.setNome("Descanso");
+            listaTimes.add(descanso);
+        }
+
+        int numeroTimes = listaTimes.size();
+        int numeroRodadas = numeroTimes - 1;
+
+        for (int rodadaIndex = 0; rodadaIndex < numeroRodadas; rodadaIndex++) {
+            Rodada rodada = new Rodada();
+            rodada.setTabela(this);
+
+            for (int i = 0; i < numeroTimes / 2; i++) {
+                Clube time1 = listaTimes.get(i);
+                Clube time2 = listaTimes.get(numeroTimes - 1 - i);
+
+                if (!time1.getNome().equals("Descanso") && !time2.getNome().equals("Descanso")) {
+                    Partida partida = new Partida();
+                    partida.setTimeCasa(time1);
+                    partida.setTimeVisitante(time2);
+                    partida.setRodada(rodada);
+                    rodada.getPartidas().add(partida);
+                }
+            }
+
+            rodadas.add(rodada);
+
+            Clube timeRemovido = listaTimes.remove(1);
+            listaTimes.add(timeRemovido);
+        }
+    }
+}
